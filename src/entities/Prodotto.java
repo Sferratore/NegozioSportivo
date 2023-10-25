ec48fdfba5bb4b9594085631876afc3da1e3cf34
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-10-25 10:32:25 +0200

    Prime classi push

---
+++ src/entities/Prodotto.java
@@ -0,0 +1,76 @@
+package entities;
+
+/**
+ * Classe che rappresenta i prodotti di un negozio sportivo.
+ */
+public class Prodotto {
+	private int id; // PK
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private int quantità;
+	
+	// Costruttori
+	public Prodotto() {
+
+	}
+	
+	public Prodotto(int id, String nome, String descrizione, double prezzo, int quantità) {
+		this.id = id;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.quantità = quantità;
+	}
+	
+	// Metodi
+
+	
+	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public int getQuantità() {
+		return quantità;
+	}
+
+	public void setQuantità(int quantità) {
+		this.quantità = quantità;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Prodotto [id=" + id + ", nome=" + nome + ", descrizione=" + descrizione + ", prezzo=" + prezzo
+				+ ", quantità=" + quantità + "]";
+	}
+}
---
+++ src/entities/Utente.java
@@ -0,0 +1,112 @@
+package entities;
+
+import java.util.ArrayList;
+
+/**
+ * Classe che rappresenta l'utente di un negozio sportivo.
+ */
+public class Utente {
+	private String username; // PK
+	private String email;
+	private String password;
+	private String nome;
+	private String cognome;
+	private ArrayList<Prodotto> carrello = new ArrayList<>();
+	private boolean isAdmin;
+	
+	// Costruttori
+	public Utente() {
+		
+	}
+	
+	public Utente(String username, String email, String password, String nome, String cognome,
+			boolean isAdmin) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.isAdmin = isAdmin;
+	}
+	
+	public Utente(String username, String email, String password, String nome, String cognome,
+			ArrayList<Prodotto> carrello, boolean isAdmin) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.isAdmin = isAdmin;
+		this.carrello = carrello;
+	}
+
+	// Metodi
+	
+	
+	// Getters & Setters
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public ArrayList<Prodotto> getCarrello() {
+		return carrello;
+	}
+
+	public void setCarrello(ArrayList<Prodotto> carrello) {
+		this.carrello = carrello;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Utente [username=" + username + ", email=" + email + ", password=" + password + ", nome=" + nome
+				+ ", cognome=" + cognome + ", carrello=" + carrello + ", isAdmin=" + isAdmin + "]";
+	}
+	
+	
+	
+}
